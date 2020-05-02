2cd6eb54c8739e200ce0f6d1e8e62eecc796d656
JimQing <jimqing@yeah.net>
2020-05-02 11:51:50 +0800

    code fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <loading v-if="loading" />
-  <codemirror-editor v-else/>
+    <loading v-if="loading" />
+    <codemirror-editor v-else />
 </template>
 
 <script>
@@ -8,30 +8,30 @@ import Loading from './components/Loading'
 import CodemirrorEditor from './components/CodemirrorEditor'
 import prettyPrint from 'prettify'
 export default {
-  name: 'App',
-  components: {
-    Loading,
-    CodemirrorEditor
-  },
-  data () {
-    return {
-      loading: true
+    name: 'App',
+    components: {
+        Loading,
+        CodemirrorEditor
+    },
+    data() {
+        return {
+            loading: true
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.loading = false
+        }, 200)
+        window.console &&
+            window.console.log &&
+            (console.log("Think big, train fast, learn deep. See https://github.com/yanglbme"))
+        setTimeout(() => {
+            // document.body.addEventListener('load', prettyPrint())
+        }, 2000)
     }
-  },
-  mounted () {
-    setTimeout(() => {
-      this.loading = false
-    }, 200)
-        window.console
-      && window.console.log
-      && (console.log("Think big, train fast, learn deep. See https://github.com/yanglbme"))
-    setTimeout(() => {
-      // document.body.addEventListener('load', prettyPrint())
-    }, 2000)
-  }
 }
+
 </script>
 
 <style lang="scss" scoped>
-
 </style>
