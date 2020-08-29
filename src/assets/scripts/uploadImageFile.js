ec2ae4aa51abf973a7830fc0cc7d7ac040d481fd
yanglbme <szuyanglb@outlook.com>
2020-08-29 19:13:52 +0800

    fix: update upload method

--- src/assets/scripts/uploadImageFile.js
+++ src/assets/scripts/uploadImageFile.js
@@ -3,27 +3,24 @@ const githubResourceUrl = 'raw.githubusercontent.com/filess/images/master/';
 const cdnResourceUrl = 'cdn.jsdelivr.net/gh/filess/images/';
 
 export function uploadImgFile(file) {
-        
-    return new Promise((resolve, reject)=> {
-        const checkImageResult = isImageIllegal(file);
 
+    return new Promise((resolve, reject) => {
+        const checkImageResult = isImageIllegal(file);
         if (checkImageResult) {
             reject(checkImageResult);
-        } else {
-            const imgFile = new FileReader();
-            imgFile.readAsDataURL(file);
-            
-            imgFile.onload = function() {
-                const base64Content = this.result.split(',').pop();
-                fileApi.fileUpload(base64Content, file.name).then(res=> {
-                    const imageUrl = res.content.download_url.replace(githubResourceUrl, cdnResourceUrl)
-                    
-                    resolve(imageUrl);
-                }).catch(err => {
-                    console.log(err.message)
-                })
-            }
+            return;
+        }
+        const imgFile = new FileReader();
+        imgFile.readAsDataURL(file);
 
+        imgFile.onload = function () {
+            const base64Content = this.result.split(',').pop();
+            fileApi.fileUpload(base64Content, file.name).then(res => {
+                const imageUrl = res.content.download_url.replace(githubResourceUrl, cdnResourceUrl);
+                resolve(imageUrl);
+            }).catch(err => {
+                console.log(err.message)
+            })
         }
     });
 }
