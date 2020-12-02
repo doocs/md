48fa729720da5a9d1724d4e182039bfa90c78c97
yanglbme <szuyanglb@outlook.com>
2020-12-02 19:42:06 +0800

    chore: update access token list

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,9 @@
 const githubConfig = {
     username: "filess",
-    repoList: Array.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], (e) => `img${e}`),
+    repoList: Array.from(
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+        (e) => `img${e}`
+    ),
     branch: "main",
     accessTokenList: [
         "7715d7ca67b5d3837cfdoocsmde8c38421815aa423510af",
@@ -11,12 +14,23 @@ const githubConfig = {
         "b67e9d15cb6f910492fdoocsmdac6b44d379c953bb19eff",
         "618c4dc2244ccbbc088doocsmd125d17fd31b7d06a50cf3",
         "a4b581732e1c1507458doocsmdc5b223b27dae5e2e16a55",
+        "77904db41aee57ad79bdoocsmd760f848201dac9c96fd5e",
+        "02f251cb14ac62ab100doocsmdddbfc8527d773f1f04ce1",
+        "eb321079a95ba7028d9doocsmde2e84c502dac70de7cf08",
+        "22f74fcfb071a961fa2doocsmde28dabc746f0503a15e5d",
+        "85124c2bfe7abba0938doocsmd0af7f67918b99d085a5fd",
+        "0a561b4d4bbecb2de7edoocsmdd9ba3833d11dbc5e430f5",
+        "e8a01491188d8d5a097doocsmd03ede0aad1fe9e3af24e9",
+        "36e1f420d7e5bdebd67doocsmd65463562f5f25b20b8377",
     ],
 };
 
 const giteeConfig = {
     username: "filesss",
-    repoList: Array.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], (e) => `img${e}`),
+    repoList: Array.from(
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+        (e) => `img${e}`
+    ),
     branch: "main",
     accessTokenList: [
         "ed5fc9866bd6c2fdoocsmddd433f806fd2f399c",
@@ -27,6 +41,14 @@ const giteeConfig = {
         "d8c0b57500672c1doocsmd55f48b866b5ebcd98",
         "78c56eadb88e453doocsmd43ddd95753351771a",
         "03e1a688003948fdoocsmda16fcf41e6f03f1f0",
+        "c49121cf4d191fbdoocsmdd6a7877ed537e474a",
+        "adfeb2fadcdc4aadoocsmdfe1ee869ac9c968ff",
+        "116c94549ca4a0ddoocsmd192653af5c0694616",
+        "ecf30ed7f2eb184doocsmd51ea4ec8300371d9e",
+        "5837cf2bd5afd93doocsmd73904bed31934949e",
+        "b5b7e1c7d57e01fdoocsmd5266f552574297d78",
+        "684d55564ffbd0bdoocsmd7d747e5cc23aed6d6",
+        "3fc04a9d272ab71doocsmd010c56cb57d88d2ba",
     ],
 };
 
