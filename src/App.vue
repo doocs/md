616d2d3621c5dd9b104061ab5f9ad8d86aa5eb87
JimQing <jimqing@yeah.net>
2021-02-16 22:26:38 +0800

    fix: bug fix

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,64 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
+
+::-webkit-scrollbar {
+  width: 6px;
+  height: 6px;
+  background-color: #fff;
+}
+
+::-webkit-scrollbar-track {
+  border-radius: 6px;
+  background-color: rgba(200, 200, 200, 0.3);
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 6px;
+  background-color: rgba(144, 146, 152, 0.5);
+  transition: background-color 0.3s;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(144, 146, 152, 0.5);
+}
+/* CSS-hints */
+.CodeMirror-hints {
+	position: absolute;
+	z-index: 10;
+	overflow: hidden;
+	list-style: none;
+
+	margin: 0;
+	padding: 2px;
+
+	
+	border-radius: 4px;
+    background-color: #ffffff;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+    min-width: 200px;
+    font-size: 12px;
+	font-family: monospace;
+
+	max-height: 20em;
+	overflow-y: auto;
+    color: #333333;
+}
+
+.CodeMirror-hint {
+	margin: 0;
+    margin-top: 10px;
+	padding: 4px 6px;
+	border-radius: 2px;
+	white-space: pre;
+	color: black;
+	cursor: pointer;
+}
+.CodeMirror-hint:first-of-type {
+	margin-top: 0;
+}
+.CodeMirror-hint:hover {
+	background: #f0f0f0;
+}
 </style>
