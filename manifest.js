6698d0cdda00d14d74c94eeb220aa740452d500e
jimqing <jimqing@yeah.net>
2021-02-28 16:57:58 +0800

    feat: dynamic publicPath

---
+++ manifest.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const manifest = require("./src/manifest.json");
+
+manifest.h5.publicPath = process.env.SERVER_ENV !== "NETLIFY" ? "/md/" : "/";
+const result = JSON.stringify(manifest, null, 2);
+
+fs.writeFile("./src/manifest.json", result, function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
