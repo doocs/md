3d169ffe7e70ca16b90292eaef3366c1db2c3554
jimqing <jimqing@yeah.net>
2021-02-28 16:20:26 +0800

    fix: JSON format

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ function writeManifestJson() {
 
     manifest.h5.publicPath =
       process.env.SERVER_ENV !== "NETLIFY" ? "/md/" : "/";
-    const result = JSON.stringify(manifest, null, 4);
+    const result = JSON.stringify(manifest, null, 2);
 
     fs.writeFile("./src/manifest.json", result, function (err) {
       if (err) {
