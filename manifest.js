3364b95c6d39fc153e3e637080b854f428e5a888
KnowsCount <knowscount@gmail.com>
2021-05-16 20:16:32 +0800

    chore: optimise electron scripts (resolve #65)

--- manifest.js
+++ manifest.js
@@ -1,10 +1,32 @@
 const fs = require("fs");
 const manifest = require("./src/manifest.json");
+const package = require("./package.json");
 
-manifest.h5.publicPath = process.env.SERVER_ENV !== "NETLIFY" ? "/md/" : "/";
+switch (process.env.SERVER_ENV) {
+  case "NETLIFY":
+    manifest.h5.publicPath = "/";
+    package.main = "main.js";
+    break;
+  case "ELECTRON":
+    manifest.h5.publicPath = "/";
+    package.main = "background.js";
+    break;
+  default:
+    manifest.h5.publicPath = "/md/";
+    package.main = "main.js";
+    break;
+}
+
 const result = JSON.stringify(manifest, null, 2);
 
 fs.writeFile("./src/manifest.json", result, function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
+const packageRes = JSON.stringify(package, null, 2);
+
+fs.writeFile("./package.json", packageRes, function (err) {
   if (err) {
     console.error(err);
   }
--- src/background.js
+++ src/background.js
@@ -1,21 +1,18 @@
 /*
  * @Date: 16/05/2021 10.47.02 +0800
  * @Author: KnowsCount
- * @LastEditTime: 16/05/2021 10.59.32 +0800
+ * @LastEditTime: 16/05/2021 20.15.36 +0800
  * @FilePath: /md/src/background.js
  */
-
-'use strict'
-
-import { app, protocol, BrowserWindow } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+import { app, protocol, BrowserWindow } from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
+  { scheme: "app", privileges: { secure: true, standard: true } },
+]);
 
 async function createWindow() {
   // Create the browser window.
@@ -23,65 +20,64 @@ async function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+    },
+  });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    win.loadURL("app://./index.html");
   }
 }
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS)
+      await installExtension(VUEJS_DEVTOOLS);
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  createWindow()
-})
+  createWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
