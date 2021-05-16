661ef924a42f09929cd748fe3ddc50fd17aa5854
jimqing <jimqing@yeah.net>
2021-05-16 15:19:32 +0800

    feat: electron

--- manifest.js
+++ manifest.js
@@ -1,7 +1,22 @@
 const fs = require("fs");
 const manifest = require("./src/manifest.json");
+const package = require("./package.json");
 
-manifest.h5.publicPath = process.env.SERVER_ENV !== "NETLIFY" ? "/md/" : "/";
+switch (process.env.SERVER_ENV) {
+  case 'NETLIFY':
+    manifest.h5.publicPath = '/md/';
+    package.main = 'main.js';
+    break;
+  case 'ELECTRON':
+    manifest.h5.publicPath = '/';
+    package.main = 'electron.js';
+    break;
+  default:
+    manifest.h5.publicPath = '/';
+    package.main = 'main.js';
+    break;
+}
+
 const result = JSON.stringify(manifest, null, 2);
 
 fs.writeFile("./src/manifest.json", result, function (err) {
