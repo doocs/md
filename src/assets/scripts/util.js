55c9bc2c9644d575dda8dfb753657c32d16b5c91
yanglbme <yanglbme@users.noreply.github.com>
2021-10-26 14:46:03 +0000

    style: prettify code

--- src/assets/scripts/util.js
+++ src/assets/scripts/util.js
@@ -88,14 +88,8 @@ export function customCssWithTemplate(jsonString, color, theme) {
     custom_theme.inline.wx_link,
     jsonString.wx_link
   );
-  custom_theme.block.ul = Object.assign(
-    custom_theme.block.ul,
-    jsonString.ul
-  );
-  custom_theme.block.ol = Object.assign(
-    custom_theme.block.ol,
-    jsonString.ol
-  );
+  custom_theme.block.ul = Object.assign(custom_theme.block.ul, jsonString.ul);
+  custom_theme.block.ol = Object.assign(custom_theme.block.ol, jsonString.ol);
   custom_theme.inline.listitem = Object.assign(
     custom_theme.inline.listitem,
     jsonString.li
