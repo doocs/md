f31ceb0ecfef8676a68fbf73a3c2612f9c5be11e
yanglbme <szuyanglb@outlook.com>
2021-11-07 21:54:15 +0800

    chore: remove running logs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<script>
-export default {
-  onLaunch: function () {
-    console.log("App Launch");
-  },
-  onShow: function () {
-    console.log("App Show");
-  },
-  onHide: function () {
-    console.log("App Hide");
-  },
-};
-</script>
-
 <style lang="less">
 /* 每个页面公共css */
 @import url("./assets/less/style-mirror.css");
