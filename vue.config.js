656dd79b13185a262f31a30508c703c1dc253a90
xw <xwdaysme@gmail.com>
2021-11-24 19:41:53 +0800

    refactor: handle the default text to avoid unnecessary escaping in js (#94)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   publicPath: process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`, // 基本路径, 建议以绝对路径跟随访问目录
+  configureWebpack: (config) => {
+    config.module.rules.push({
+      test: /\.(txt|md)$/i,
+      use: [{
+        loader: 'raw-loader',
+      }]
+    })
+  },
   css: {
     sourceMap: true,
   },
