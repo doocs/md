a7a949996cb3dedd747f98663f9681ba4df77281
yanglbme <szuyanglb@outlook.com>
2021-11-30 00:03:10 +0800

    feat: reset style only

--- src/pages/index/view/CodemirrorEditor.vue
+++ src/pages/index/view/CodemirrorEditor.vue
@@ -392,7 +392,7 @@ export default {
     },
     onMenuEvent(type, info = {}) {
       switch (type) {
-        case "pageReset":
+        case "resetStyle":
           this.$refs.header.showResetConfirm = true;
           break;
         case "insertPic":
