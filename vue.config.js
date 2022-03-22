dba40a725408207f2e041cf9b5ff8832c86a5745
xw <xwdaysme@gmail.com>
2022-03-22 09:58:22 +0800

    chore: Formatting issues do not hinder compilation (#142)

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === `production`
 
 module.exports = {
+  lintOnSave: true,
   publicPath: process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`, // 基本路径, 建议以绝对路径跟随访问目录
   configureWebpack: (config) => {
     config.module.rules.push({
