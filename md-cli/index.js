19d87ce67b709edfa85065e86c994f8039cdcd3e
xw <xwdaysme@gmail.com>
2022-08-04 10:26:40 +0800

    fix(cli): More suitable service detection method (#169)

--- md-cli/index.js
+++ md-cli/index.js
@@ -25,20 +25,18 @@ new Promise(async () => {
   }).map(([key, val]) => `${key}=${val}`).join(` `)
   const cliArg = [handleSpace(`${__dirname}/node_modules/mockm/run.js`), `--log-line`, line]
   spawn(`node`, cliArg)
-  setTimeout(async () => {
-    console.log(``)
-    console.log(`doocs/md-cli v${require(`./package.json`).version}`)
-    console.log(``)
-    try {
-      if(await portIsOk(port) === true) {
-        throw new Error(`服务 ${port} 初始化失败`)
-      }
+  let oldTime = Date.now()
+  console.log(`服务启动中...`)
+  console.log(`doocs/md-cli v${require(`./package.json`).version}`)
+  let timer = setInterval(async () => {
+    if((await portIsOk(port)) !== true) { // 服务启动成功
+      clearInterval(timer)
       console.log(`服务已启动:`)
       console.log(`打开链接 ${colors.green(`http://127.0.0.1:${port}/md/`)} 即刻使用吧~`)
-    } catch (error) {
-      console.error(`启动错误 ${error}`)
+    } else if(Date.now() - oldTime > 10 * 1e3) {
+      clearInterval(timer)
+      console.log(`服务 ${port} 初始化失败, 请重试.`)
       process.exit()
     }
-    console.log(``)
-  }, 3*1e3);
+  }, 1e3);
 }).catch(err => console.log(err))
