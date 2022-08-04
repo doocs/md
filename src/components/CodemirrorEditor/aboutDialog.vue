e46a6bee16b731dad74e877f83a0ee056348643d
YangFong <yangfong2022@qq.com>
2022-08-04 15:22:01 +0800

    pref: optimizing css
    - 删除不会生效的 CSS
    - 调整结构，减少不必要的标签

--- src/components/CodemirrorEditor/aboutDialog.vue
+++ src/components/CodemirrorEditor/aboutDialog.vue
@@ -7,17 +7,15 @@
     width="30%"
     center
   >
-    <div style="text-align: center">
+    <div style="text-align: center;">
       <h3>一款高度简洁的微信 Markdown 编辑器</h3>
-    </div>
-    <div style="text-align: center; margin-top: 10px">
       <p>扫码关注我的公众号，原创技术文章第一时间推送！</p>
       <img
         src="https://cdn-doocs.oss-cn-shenzhen.aliyuncs.com/gh/doocs/md/images/1648303220922-7e14aefa-816e-44c1-8604-ade709ca1c69.png"
-        style="width: 40%; display: block; margin: 20px auto 10px"
+        style="width: 40%;"
       />
     </div>
-    <span slot="footer" class="dialog-footer">
+    <template slot="footer">
       <el-button
         type="primary"
         @click="onRedirect('https://github.com/doocs/md')"
@@ -32,7 +30,7 @@
       >
         Gitee 仓库
       </el-button>
-    </span>
+    </template>
   </el-dialog>
 </template>
 
