1e70e0a9bb69a1168390c09eea06ef22fd802257
YangFong <yangfong2022@qq.com>
2024-07-06 19:21:06 +0800

    chore: update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  unocss: true,
+  typescript: true,
+  ignores: [`src/assets/scripts/renderers`],
+  rules: {
+    'semi': [`error`, `never`],
+    'quotes': [`error`, `backtick`],
+    'no-unused-vars': `off`,
+    'no-console': `off`,
+    'no-debugger': `off`,
+  },
+})
