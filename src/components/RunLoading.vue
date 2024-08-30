58ca8e8697991959bca63827e0c0805ee225818a
YangFong <yangfong2022@qq.com>
2024-08-30 13:53:52 +0800

    feat: add loading spinner during page load (#372)

--- src/components/RunLoading.vue
+++ src/components/RunLoading.vue
@@ -16,9 +16,7 @@ onMounted(() => {
       v-if="loading"
       class="loading"
     >
-      <el-text size="large" tag="strong">
-        致力于让 Markdown 编辑更简单
-      </el-text>
+      <strong>致力于让 Markdown 编辑更简单</strong>
     </div>
   </transition>
 </template>
