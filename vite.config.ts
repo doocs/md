1a0eef8fffbaf910779f3d1701f724c2ea2b6c9b
YangFong <yangfong2022@qq.com>
2024-08-31 09:22:55 +0800

    chore: update vite.config.ts and add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,45 +2,36 @@ import path from 'node:path'
 import process from 'node:process'
 
 import { defineConfig } from 'vite'
-import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProd = mode === `production`
-
-  return {
-    base: process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`, // 基本路径, 建议以绝对路径跟随访问目录
-    define: {
-      process,
-    },
-    plugins: [
-      vue(),
-      UnoCSS(),
-      nodePolyfills({
-        include: [`path`, `util`, `timers`, `stream`, `fs`],
-        overrides: {
-          // Since `fs` is not supported in browsers, we can use the `memfs` package to polyfill it.
-          // fs: 'memfs',
-        },
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, `./src`),
+export default defineConfig({
+  base: process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`, // 基本路径, 建议以绝对路径跟随访问目录
+  define: {
+    process,
+  },
+  plugins: [
+    vue(),
+    UnoCSS(),
+    vueDevTools(),
+    nodePolyfills({
+      include: [`path`, `util`, `timers`, `stream`, `fs`],
+      overrides: {
+        // Since `fs` is not supported in browsers, we can use the `memfs` package to polyfill it.
+        // fs: 'memfs',
       },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, `./src`),
     },
-    css: {
-      devSourcemap: !isProd,
-      postcss: {
-        plugins: [tailwind(), autoprefixer()],
-      },
-    },
-    build: {
-      sourcemap: !isProd,
-    },
-  }
-})
+  },
+  css: {
+    devSourcemap: true,
+  },
+},
+)
