e6be8f64645d3dcad152ab5665f07f4cf7c89307
honwhy.wang <honwhy.wang@hotmail.com>
2024-09-02 22:29:51 +0800

    feat: use unplugin to minize element-plus size

--- src/element/index.js
+++ src/element/index.js
@@ -1,11 +1,11 @@
-import ElementPlus, { ElLoading, ElMessage } from 'element-plus'
+import { ElLoading, ElMessage } from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 export default {
   install(app) {
-    app.use(ElementPlus, { size: `default` })
+    // app.use(ElementPlus, { size: `default` })
 
     app.config.globalProperties.$loading = ElLoading.service
     app.config.globalProperties.$message = ElMessage
