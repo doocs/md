2f9a3e2e86c0a64368ba2821982564d13cce4209
yanglbme <szuyanglb@outlook.com>
2024-09-02 20:12:48 +0800

    chore: update renderer

--- src/utils/renderer.ts
+++ src/utils/renderer.ts
@@ -1,4 +1,4 @@
-import type { Renderer, Tokens } from 'marked'
+import type { Renderer, RendererObject, Tokens } from 'marked'
 import { marked } from 'marked'
 import hljs from 'highlight.js'
 import markedKatex from 'marked-katex-extension'
@@ -64,8 +64,8 @@ export function initRenderer(opts: IOpts) {
   let styleMapping = buildTheme(opts)
 
   function styledContent(styleLabel: string, content: string, tagName?: string) {
-    const tag = tagName ?? styleLabel;
-    return `<${tag} ${getStyles(styleLabel)}>${content}</${tag}>`;
+    const tag = tagName ?? styleLabel
+    return `<${tag} ${getStyles(styleLabel)}>${content}</${tag}>`
   }
 
   function addFootnote(title: string, link: string) {
@@ -120,156 +120,156 @@ export function initRenderer(opts: IOpts) {
   let listIndex = 0
   let isOrdered = false
 
-  marked.use({
-    renderer: {
-      heading({ tokens, depth }) {
-        const text = this.parser.parseInline(tokens)
-        const tag = `h${depth}`
-        return styledContent(tag, text)
-      },
-
-      paragraph({ tokens }) {
-        const text = this.parser.parseInline(tokens)
-        const isFigureImage = text.includes(`<figure`) && text.includes(`<img`)
-        const isEmpty = text.trim() === ``
-        if (isFigureImage || isEmpty) {
-          return text
-        }
-
-        return styledContent(`p`, text)
-      },
-
-      blockquote({ tokens }) {
-        let text = this.parser.parse(tokens)
-        text = text.replace(/<p.*?>/g, `<p ${getStyles(`blockquote_p`)}>`)
-        return styledContent(`blockquote`, text)
-      },
-
-      code({ text, lang = `` }) {
-        if (lang.startsWith(`mermaid`)) {
-          clearTimeout(codeIndex)
-          codeIndex = setTimeout(() => {
-            mermaid.run()
-          }, 0) as any as number
-          return `<pre class="mermaid">${text}</pre>`
-        }
-        const langText = lang.split(` `)[0]
-        const language = hljs.getLanguage(langText) ? langText : `plaintext`
-        let highlighted = hljs.highlight(text, { language }).value
-        highlighted = highlighted
-          .replace(/\r\n/g, `<br/>`)
-          .replace(/\n/g, `<br/>`)
-          .replace(/(>[^<]+)|(^[^<]+)/g, str => str.replace(/\s/g, `&nbsp;`))
-
-        return `<pre class="hljs code__pre" ${getStyles(
-          `code_pre`,
-        )}><code class="language-${lang}" ${getStyles(
-          `code`,
-        )}>${highlighted}</code></pre>`
-      },
-
-      codespan({ text }: Tokens.Codespan) {
-        return styledContent(`codespan`, text, `code`)
-      },
-
-      listitem(token) {
-        const prefix = isOrdered ? `${listIndex + 1}. ` : `• `
-        // TODO 预备支持 list
-        // if (token.checked != null) {
-        //   prefix = `<input checked="${token.checked}" disabled type="checkbox"> `
-        // }
-        // TODO 写的太烂，需要重构
-        return `<li ${getStyles(`listitem`)}>${prefix}${token.tokens.map(t => (this[t.type as keyof Renderer] as <T>(token: T) => string)(t)).join(``)}</li>`
-      },
-
-      // TODO
-      list({ ordered, items }) {
-        const listItems = []
-        for (let i = 0; i < items.length; i++) {
-          isOrdered = ordered
-          listIndex = i
-          const item = items[i]
-          listItems.push(this.listitem(item))
-        }
-
-        const label = ordered ? `ol` : `ul`
-        return styledContent(label, listItems.join(``))
-      },
-
-      image({ href, title, text }) {
-        const createSubText = (s: string | null) =>
-          s ? `<figcaption ${getStyles(`figcaption`)}>${s}</figcaption>` : ``
-        const transform
-      = {
-        'alt': () => text,
-        'title': () => title,
-        'alt-title': () => text || title,
-        'title-alt': () => title || text,
-      }[opts.legend] || (() => ``)
-
-        const subText = createSubText(transform())
-        const figureStyles = getStyles(`figure`)
-        const imgStyles = getStyles(`image`)
-        return `<figure ${figureStyles}><img ${imgStyles} src="${href}" title="${title}" alt="${text}"/>${subText}</figure>`
-      },
-
-      link({ href, title, text }) {
-        if (href.startsWith(`https://mp.weixin.qq.com`)) {
-          return `<a href="${href}" title="${title || text}" ${getStyles(
-            `wx_link`,
-          )}>${text}</a>`
-        }
-        if (href === text) {
-          return text
-        }
-        if (opts.status) {
-          const ref = addFootnote(title || text, href)
-          return `<span ${getStyles(
-            `link`,
-          )}>${text}<sup>[${ref}]</sup></span>`
-        }
-        return styledContent(`link`, text, `span`)
-      },
-
-      strong({ text }) {
-        return styledContent(`strong`, text)
-      },
-
-      em({ text }) {
-        return `<span style="font-style: italic;">${text}</span>`
-      },
-
-      table({ header, rows }) {
-        const headerRow = header
-          .map(cell => styledContent(`td`, cell.text))
-          .join(``)
-        const body = rows
-          .map((row) => {
-            const rowContent = row
-              .map(cell => styledContent(`td`, cell.text))
-              .join(``)
-            return styledContent(`tr`, rowContent)
-          })
-          .join(``)
-        return `
-          <section style="padding:0 8px;">
-            <table class="preview-table">
-              <thead ${getStyles(`thead`)}>${headerRow}</thead>
-              <tbody>${body}</tbody>
-            </table>
-          </section>
-        `
-      },
-
-      tablecell({ text }) {
-        return styledContent(`td`, text)
-      },
-
-      hr() {
-        return styledContent(`hr`, ``)
-      },
+  const renderer: RendererObject = {
+    heading({ tokens, depth }) {
+      const text = this.parser.parseInline(tokens)
+      const tag = `h${depth}`
+      return styledContent(tag, text)
     },
-  })
+
+    paragraph({ tokens }) {
+      const text = this.parser.parseInline(tokens)
+      const isFigureImage = text.includes(`<figure`) && text.includes(`<img`)
+      const isEmpty = text.trim() === ``
+      if (isFigureImage || isEmpty) {
+        return text
+      }
+
+      return styledContent(`p`, text)
+    },
+
+    blockquote({ tokens }) {
+      let text = this.parser.parse(tokens)
+      text = text.replace(/<p.*?>/g, `<p ${getStyles(`blockquote_p`)}>`)
+      return styledContent(`blockquote`, text)
+    },
+
+    code({ text, lang = `` }) {
+      if (lang.startsWith(`mermaid`)) {
+        clearTimeout(codeIndex)
+        codeIndex = setTimeout(() => {
+          mermaid.run()
+        }, 0) as any as number
+        return `<pre class="mermaid">${text}</pre>`
+      }
+      const langText = lang.split(` `)[0]
+      const language = hljs.getLanguage(langText) ? langText : `plaintext`
+      let highlighted = hljs.highlight(text, { language }).value
+      highlighted = highlighted
+        .replace(/\r\n/g, `<br/>`)
+        .replace(/\n/g, `<br/>`)
+        .replace(/(>[^<]+)|(^[^<]+)/g, str => str.replace(/\s/g, `&nbsp;`))
+
+      return `<pre class="hljs code__pre" ${getStyles(
+        `code_pre`,
+      )}><code class="language-${lang}" ${getStyles(
+        `code`,
+      )}>${highlighted}</code></pre>`
+    },
+
+    codespan({ text }: Tokens.Codespan) {
+      return styledContent(`codespan`, text, `code`)
+    },
+
+    listitem(token) {
+      const prefix = isOrdered ? `${listIndex + 1}. ` : `• `
+      // TODO 预备支持 list
+      // if (token.checked != null) {
+      //   prefix = `<input checked="${token.checked}" disabled type="checkbox"> `
+      // }
+      // TODO 写的太烂，需要重构
+      return `<li ${getStyles(`listitem`)}>${prefix}${token.tokens.map(t => (this[t.type as keyof Renderer] as <T>(token: T) => string)(t)).join(``)}</li>`
+    },
+
+    // TODO
+    list({ ordered, items }) {
+      const listItems = []
+      for (let i = 0; i < items.length; i++) {
+        isOrdered = ordered
+        listIndex = i
+        const item = items[i]
+        listItems.push(this.listitem(item))
+      }
+
+      const label = ordered ? `ol` : `ul`
+      return styledContent(label, listItems.join(``))
+    },
+
+    image({ href, title, text }) {
+      const createSubText = (s: string | null) =>
+        s ? `<figcaption ${getStyles(`figcaption`)}>${s}</figcaption>` : ``
+      const transform
+    = {
+      'alt': () => text,
+      'title': () => title,
+      'alt-title': () => text || title,
+      'title-alt': () => title || text,
+    }[opts.legend] || (() => ``)
+
+      const subText = createSubText(transform())
+      const figureStyles = getStyles(`figure`)
+      const imgStyles = getStyles(`image`)
+      return `<figure ${figureStyles}><img ${imgStyles} src="${href}" title="${title}" alt="${text}"/>${subText}</figure>`
+    },
+
+    link({ href, title, text }) {
+      if (href.startsWith(`https://mp.weixin.qq.com`)) {
+        return `<a href="${href}" title="${title || text}" ${getStyles(
+          `wx_link`,
+        )}>${text}</a>`
+      }
+      if (href === text) {
+        return text
+      }
+      if (opts.status) {
+        const ref = addFootnote(title || text, href)
+        return `<span ${getStyles(
+          `link`,
+        )}>${text}<sup>[${ref}]</sup></span>`
+      }
+      return styledContent(`link`, text, `span`)
+    },
+
+    strong({ text }) {
+      return styledContent(`strong`, text)
+    },
+
+    em({ text }) {
+      return `<span style="font-style: italic;">${text}</span>`
+    },
+
+    table({ header, rows }) {
+      const headerRow = header
+        .map(cell => styledContent(`td`, cell.text))
+        .join(``)
+      const body = rows
+        .map((row) => {
+          const rowContent = row
+            .map(cell => styledContent(`td`, cell.text))
+            .join(``)
+          return styledContent(`tr`, rowContent)
+        })
+        .join(``)
+      return `
+        <section style="padding:0 8px;">
+          <table class="preview-table">
+            <thead ${getStyles(`thead`)}>${headerRow}</thead>
+            <tbody>${body}</tbody>
+          </table>
+        </section>
+      `
+    },
+
+    tablecell({ text }) {
+      return styledContent(`td`, text)
+    },
+
+    hr() {
+      return styledContent(`hr`, ``)
+    },
+  }
+
+  marked.use({ renderer })
 
   return {
     buildAddition,
