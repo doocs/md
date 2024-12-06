2bb4e90f27459ba9a59a46e789c66e5bbefcf3e3
YangFong <yangfong2022@gmail.com>
2024-12-06 09:41:29 +0800

    fix: type error

--- src/components/CodemirrorEditor/UploadImgDialog.vue
+++ src/components/CodemirrorEditor/UploadImgDialog.vue
@@ -257,7 +257,7 @@ function beforeImageUpload(file: File) {
   // check image
   const checkResult = checkImage(file)
   if (!checkResult.ok) {
-    toast.error(checkResult.msg)
+    toast.error(checkResult.msg || ``)
     return false
   }
   // check image host
