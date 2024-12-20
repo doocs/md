1d6ab54091f33f5dcc19cb1aade29934221b84ab
Libin YANG <szuyanglb@outlook.com>
2024-12-20 09:14:56 +0800

    feat: set index after deleting post (#483)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -73,7 +73,7 @@ export const useStore = defineStore(`store`, () => {
 
   const delPost = (index: number) => {
     posts.value.splice(index, 1)
-    currentPostIndex.value = posts.value.length - 1
+    currentPostIndex.value = Math.min(index, posts.value.length - 1)
   }
 
   watch(currentPostIndex, () => {
