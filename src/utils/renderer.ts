29090df419c10405b8ede15b045ea3b0c2861ff2
yanglbme <szuyanglb@outlook.com>
2024-12-24 11:45:38 +0800

    fix: render tab to spaces

--- src/utils/renderer.ts
+++ src/utils/renderer.ts
@@ -174,6 +174,8 @@ export function initRenderer(opts: IOpts) {
       const langText = lang.split(` `)[0]
       const language = hljs.getLanguage(langText) ? langText : `plaintext`
       let highlighted = hljs.highlight(text, { language }).value
+      // tab to 4 spaces
+      highlighted = highlighted.replace(/\t/g, '    ')
       highlighted = highlighted
         .replace(/\r\n/g, `<br/>`)
         .replace(/\n/g, `<br/>`)
