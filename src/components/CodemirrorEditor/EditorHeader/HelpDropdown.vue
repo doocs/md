9660c7722b6758e856177565c2e594f123fd5755
Libin YANG <szuyanglb@outlook.com>
2025-04-12 10:52:49 +0800

    feat: add fund component (#626)

--- src/components/CodemirrorEditor/EditorHeader/HelpDropdown.vue
+++ src/components/CodemirrorEditor/EditorHeader/HelpDropdown.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const aboutDialogVisible = ref(false)
+const fundDialogVisible = ref(false)
 </script>
 
 <template>
@@ -11,8 +12,12 @@ const aboutDialogVisible = ref(false)
       <MenubarCheckboxItem @click="aboutDialogVisible = true">
         <span>关于</span>
       </MenubarCheckboxItem>
+      <MenubarCheckboxItem @click="fundDialogVisible = true">
+        <span>赞赏</span>
+      </MenubarCheckboxItem>
     </MenubarContent>
   </MenubarMenu>
 
   <AboutDialog :visible="aboutDialogVisible" @close="aboutDialogVisible = false" />
+  <FundDialog :visible="fundDialogVisible" @close="fundDialogVisible = false" />
 </template>
