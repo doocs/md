6125360b6d991e51b1f2268955409bc95102359a
Libin YANG <szuyanglb@outlook.com>
2025-04-20 08:12:03 +0800

    fix: sort mode

--- src/components/CodemirrorEditor/PostSlider.vue
+++ src/components/CodemirrorEditor/PostSlider.vue
@@ -170,10 +170,10 @@ const sortMode = useStorage(addPrefix(`sort_mode`), `create-old-new`)
           }
         }).sort((a, b) => {
           if (sortMode === 'A-Z') {
-            return a.title < b.title ? 1 : -1
+            return a.title > b.title ? 1 : -1
           }
           if (sortMode === 'Z-A') {
-            return a.title > b.title ? 1 : -1
+            return a.title < b.title ? 1 : -1
           }
           if (sortMode === 'update-new-old') {
             return new Date(a.updateDatetime) < new Date(b.updateDatetime) ? 1 : -1
