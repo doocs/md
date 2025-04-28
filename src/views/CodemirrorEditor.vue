ba63ba75babd59201998c7b88ba55e02a9de12ee
Nick Wu <38874640+wNing50@users.noreply.github.com>
2025-04-28 11:42:31 +0800

    fix: code style in `grace` and `simlpe` theme (#667)
    close #611

--- src/views/CodemirrorEditor.vue
+++ src/views/CodemirrorEditor.vue
@@ -486,7 +486,7 @@ const isOpenHeadingSlider = ref(false)
                 </div>
               </div>
             </div>
-            <BackTop target="preview" :right="20" :bottom="20" />
+            <BackTop target="preview" :right="isMobile ? 24 : 20" :bottom="isMobile ? 90 : 20" />
           </div>
           <div
             class="bg-background absolute left-0 top-0 border rounded-2 rounded-lt-none p-2 text-sm shadow"
