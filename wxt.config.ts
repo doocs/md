5bdba33fe3ae44b3649aae21a071d63a2792b1da
Honwhy Wang <honwhy.wang@hotmail.com>
2025-04-28 22:10:19 +0800

    fix: remove description from manifest in wxt.config.ts (#670)

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   extensionApi: `chrome`,
   manifest: {
     name: `公众号内容编辑器`,
-    description: `一款高度简洁的微信 Markdown 编辑器：支持 Markdown 语法、色盘取色、多图上传、一键下载文档、自定义 CSS 样式、一键重置、微信公众号图床等特性`,
     icons: {
       256: `/mpmd/icon-256.png`,
     },
