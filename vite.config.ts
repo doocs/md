98b10dee37c6282e17be424a82b79b32f28781c8
YangFong <yangfong2022@gmail.com>
2025-08-09 04:59:17 +0800

    feat: add PWA support with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { VitePWA } from 'vite-plugin-pwa'
 import { VitePluginRadar } from 'vite-plugin-radar'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -19,6 +20,36 @@ export default defineConfig({
     vue(),
     UnoCSS(),
     vueDevTools(),
+    VitePWA({
+      registerType: `autoUpdate`,
+      includeAssets: [`favicon.ico`],
+      manifest: {
+        name: `@doocs-md`,
+        short_name: `@doocs-md`,
+        theme_color: `#ffffff`,
+        icons: [
+          {
+            src: `/md/pwa-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/md/pwa-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `/md/pwa-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: `any maskable`,
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
     nodePolyfills({
       include: [`path`, `util`, `timers`, `stream`, `fs`],
       overrides: {
