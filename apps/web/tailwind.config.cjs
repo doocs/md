e56cae5a2d8a0c04dbbe85fa8cb38a074887da2c
dribble-njr <wzw15292257101@163.com>
2025-08-20 14:52:59 +0800

    fix: resolve dynamic class detection in monorepo structure

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -10,10 +10,8 @@ module.exports = {
   },
 
   content: [
-    `./pages/**/*.{ts,tsx,vue}`,
-    `./components/**/*.{ts,tsx,vue}`,
-    `./app/**/*.{ts,tsx,vue}`,
     `./src/**/*.{ts,tsx,vue}`,
+    `../../packages/shared/src/**/*.{ts,tsx,vue}`,
   ],
 
   theme: {
