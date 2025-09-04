7d6630a0dfda8764e1928a3fe12a3a7a4a35d31d
Ziven <contact@ziven.me>
2025-09-04 11:33:53 +0800

    build(deps-dev): bump vite from 6.3.5 to 7.1.4

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -29,8 +29,7 @@ export default defineConfig({
     VitePluginRadar({
       analytics: { id: `G-7NZL3PZ0NK` },
     }),
-    process.env.ANALYZE === `true`
-    && visualizer({ emitFile: true, filename: `stats.html` }),
+    ...(process.env.ANALYZE === `true` ? [visualizer({ emitFile: true, filename: `stats.html` }) as any] : []),
     AutoImport({
       imports: [`vue`, `pinia`, `@vueuse/core`],
       dirs: [`./src/stores`, `./src/utils/toast`, `./src/composables`],
