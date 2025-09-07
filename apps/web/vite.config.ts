7ce612cf3ae01a8566efc5886801952cabf85f17
YangFong <yangfong2022@gmail.com>
2025-09-07 23:40:52 +0800

    fix: adjust vite PWA cache file size limit

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -48,6 +48,9 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
+      },
       devOptions: {
         enabled: true,
       },
