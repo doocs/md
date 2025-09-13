a5332c31f85e6f26ab6dee21ab3a822b669aa602
dribble-njr <wzw15292257101@163.com>
2025-09-13 17:37:07 +0800

    feat: editor header mobile adaptation

--- apps/web/src/components/CodemirrorEditor/EditorHeader/FileDropdown.vue
+++ apps/web/src/components/CodemirrorEditor/EditorHeader/FileDropdown.vue
@@ -2,6 +2,14 @@
 import { Download, FileCode, FileCog, FileText, Upload } from 'lucide-vue-next'
 import { useStore } from '@/stores'
 
+const props = withDefaults(defineProps<{
+  asSub?: boolean
+}>(), {
+  asSub: false,
+})
+
+const { asSub } = toRefs(props)
+
 const store = useStore()
 
 const {
@@ -23,7 +31,54 @@ const importMarkdownContent = useImportMarkdownContent()
 </script>
 
 <template>
-  <MenubarMenu>
+  <!-- 作为 MenubarSub 使用 -->
+  <MenubarSub v-if="asSub">
+    <MenubarSubTrigger>
+      文件
+    </MenubarSubTrigger>
+    <MenubarSubContent>
+      <MenubarItem @click="importMarkdownContent()">
+        <Upload class="mr-2 size-4" />
+        导入 .md
+      </MenubarItem>
+      <MenubarItem @click="exportEditorContent2MD()">
+        <Download class="mr-2 size-4" />
+        导出 .md
+      </MenubarItem>
+      <MenubarItem @click="exportEditorContent2HTML()">
+        <FileCode class="mr-2 size-4" />
+        导出 .html
+      </MenubarItem>
+      <MenubarItem @click="exportEditorContent2PureHTML()">
+        <FileCode class="mr-2 size-4" />
+        导出 .html（无样式）
+      </MenubarItem>
+      <MenubarItem @click="exportEditorContent2PDF()">
+        <FileText class="mr-2 size-4" />
+        导出 .pdf
+      </MenubarItem>
+      <MenubarItem @click="downloadAsCardImage()">
+        <Download class="mr-2 size-4" />
+        导出 .png
+      </MenubarItem>
+      <MenubarSeparator />
+      <MenubarItem @click="editorStateDialogVisible = true">
+        <FileCog class="mr-2 size-4" />
+        导入/导出项目配置
+      </MenubarItem>
+      <MenubarSeparator />
+      <MenubarCheckboxItem v-model:checked="isDark">
+        深色模式
+      </MenubarCheckboxItem>
+      <MenubarSeparator />
+      <MenubarCheckboxItem v-model:checked="isEditOnLeft">
+        左侧编辑
+      </MenubarCheckboxItem>
+    </MenubarSubContent>
+  </MenubarSub>
+
+  <!-- 作为 MenubarMenu 使用（默认） -->
+  <MenubarMenu v-else>
     <MenubarTrigger>
       文件
     </MenubarTrigger>
