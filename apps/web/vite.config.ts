f6efebb48e358698f1e58f3148339bc55b4abed4
Libin YANG <szuyanglb@outlook.com>
2025-10-01 12:25:50 +0800

    chore: build utools plugin (#1037)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -12,7 +12,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { VitePluginRadar } from 'vite-plugin-radar'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const base = process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`
+const isUTools = process.env.SERVER_ENV === `UTOOLS`
+const base
+  = process.env.SERVER_ENV === `NETLIFY`
+    ? `/`
+    : isUTools
+      ? `./`
+      : `/md/`
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -27,39 +33,43 @@ export default defineConfig(({ mode }) => {
       vueDevTools({
         launchEditor: env.VITE_LAUNCH_EDITOR ?? `code`,
       }),
-      VitePWA({
-        registerType: `autoUpdate`,
-        includeAssets: [`favicon.ico`],
-        manifest: {
-          name: `@doocs-md`,
-          short_name: `@doocs-md`,
-          theme_color: `#ffffff`,
-          icons: [
-            {
-              src: `${base}pwa-192x192.png`,
-              sizes: `192x192`,
-              type: `image/png`,
-            },
-            {
-              src: `${base}pwa-512x512.png`,
-              sizes: `512x512`,
-              type: `image/png`,
-            },
-            {
-              src: `${base}pwa-512x512.png`,
-              sizes: `512x512`,
-              type: `image/png`,
-              purpose: `any maskable`,
-            },
-          ],
-        },
-        workbox: {
-          maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
-        },
-        devOptions: {
-          enabled: true,
-        },
-      }),
+      ...(!isUTools
+        ? [
+            VitePWA({
+              registerType: `autoUpdate`,
+              includeAssets: [`favicon.ico`],
+              manifest: {
+                name: `@doocs-md`,
+                short_name: `@doocs-md`,
+                theme_color: `#ffffff`,
+                icons: [
+                  {
+                    src: `${base}pwa-192x192.png`,
+                    sizes: `192x192`,
+                    type: `image/png`,
+                  },
+                  {
+                    src: `${base}pwa-512x512.png`,
+                    sizes: `512x512`,
+                    type: `image/png`,
+                  },
+                  {
+                    src: `${base}pwa-512x512.png`,
+                    sizes: `512x512`,
+                    type: `image/png`,
+                    purpose: `any maskable`,
+                  },
+                ],
+              },
+              workbox: {
+                maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
+              },
+              devOptions: {
+                enabled: true,
+              },
+            }),
+          ]
+        : []),
       nodePolyfills({
         include: [`path`, `util`, `timers`, `stream`, `fs`],
         overrides: {
