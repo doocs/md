595aab31bb8a498fefe2f1e54978b844f1cec845
Libin YANG <contact@yanglibin.info>
2025-10-17 21:25:31 +0800

    chore: update

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -64,6 +64,7 @@ export default defineConfig(({ mode }) => {
               },
               workbox: {
                 maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
+                navigateFallback: null,
               },
               devOptions: {
                 enabled: true,
