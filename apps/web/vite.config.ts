7d5838f3679016cd2a83a67228ea9ccfd8d283d6
Libin YANG <contact@yanglibin.info>
2025-10-19 11:26:53 +0800

    fix: pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,7 +9,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { VitePWA } from 'vite-plugin-pwa'
 import { VitePluginRadar } from 'vite-plugin-radar'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -34,44 +33,6 @@ export default defineConfig(({ mode }) => {
       vueDevTools({
         launchEditor: env.VITE_LAUNCH_EDITOR ?? `code`,
       }),
-      ...(!isUTools
-        ? [
-            VitePWA({
-              registerType: `autoUpdate`,
-              includeAssets: [`favicon.ico`],
-              manifest: {
-                name: `@doocs-md`,
-                short_name: `@doocs-md`,
-                theme_color: `#ffffff`,
-                icons: [
-                  {
-                    src: `${base}pwa-192x192.png`,
-                    sizes: `192x192`,
-                    type: `image/png`,
-                  },
-                  {
-                    src: `${base}pwa-512x512.png`,
-                    sizes: `512x512`,
-                    type: `image/png`,
-                  },
-                  {
-                    src: `${base}pwa-512x512.png`,
-                    sizes: `512x512`,
-                    type: `image/png`,
-                    purpose: `any maskable`,
-                  },
-                ],
-              },
-              workbox: {
-                maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
-                navigateFallback: null,
-              },
-              devOptions: {
-                enabled: true,
-              },
-            }),
-          ]
-        : []),
       !isCfWorkers && nodePolyfills({
         include: [`path`, `util`, `timers`, `stream`, `fs`],
         overrides: {
