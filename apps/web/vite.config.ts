34f48a121c77ba7897ee0c5f9ecc9ef9db5a969f
Libin YANG <szuyanglb@outlook.com>
2025-10-19 11:38:55 +0800

    chore: update workflows (#1083)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,7 +9,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { VitePWA } from 'vite-plugin-pwa'
 import { VitePluginRadar } from 'vite-plugin-radar'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -34,44 +33,6 @@ export default defineConfig(({ mode }) => {
       vueDevTools({
         launchEditor: env.VITE_LAUNCH_EDITOR ?? `code`,
       }),
-      ...(!isUTools
-        ? [
-            VitePWA({
-              registerType: `autoUpdate`,
-              includeAssets: [`favicon.ico`],
-              manifest: {
-                name: `@doocs-md`,
-                short_name: `@doocs-md`,
-                theme_color: `#ffffff`,
-                icons: [
-                  {
-                    src: `${base}pwa-192x192.png`,
-                    sizes: `192x192`,
-                    type: `image/png`,
-                  },
-                  {
-                    src: `${base}pwa-512x512.png`,
-                    sizes: `512x512`,
-                    type: `image/png`,
-                  },
-                  {
-                    src: `${base}pwa-512x512.png`,
-                    sizes: `512x512`,
-                    type: `image/png`,
-                    purpose: `any maskable`,
-                  },
-                ],
-              },
-              workbox: {
-                maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
-                navigateFallback: null,
-              },
-              devOptions: {
-                enabled: true,
-              },
-            }),
-          ]
-        : []),
       !isCfWorkers && nodePolyfills({
         include: [`path`, `util`, `timers`, `stream`, `fs`],
         overrides: {
@@ -107,7 +68,6 @@ export default defineConfig(({ mode }) => {
             if (id.includes(`node_modules`)) {
               if (id.includes(`katex`))
                 return `katex`
-              // mermaid 已改为外部 CDN 依赖
               if (id.includes(`highlight.js`))
                 return `hljs`
               if (id.includes(`codemirror`))
