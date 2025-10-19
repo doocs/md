4b57949d2fbcc6b3bdd2745c249468dd748fa364
YangFong <yangfong2022@gmail.com>
2025-10-19 09:50:04 +0800

    feat: add custom panel for editer

--- apps/web/src/components/editor/RightSlider.vue
+++ apps/web/src/components/editor/RightSlider.vue
@@ -7,14 +7,12 @@ import {
   fontSizeOptions,
   legendOptions,
   themeOptions,
-  widthOptions,
 } from '@md/shared/configs'
-import { Moon, Sun, X } from 'lucide-vue-next'
+import { X } from 'lucide-vue-next'
 import PickColors from 'vue-pick-colors'
-import { useDisplayStore, useStore } from '@/stores'
+import { useStore } from '@/stores'
 
 const store = useStore()
-const displayStore = useDisplayStore()
 
 // 控制是否启用动画
 const enableAnimation = ref(false)
@@ -32,11 +30,7 @@ watch(() => store.isMobile, () => {
   enableAnimation.value = false
 })
 
-const { isDark, primaryColor } = storeToRefs(store)
-
-function customStyle() {
-  displayStore.toggleShowCssEditor()
-}
+const { primaryColor } = storeToRefs(store)
 
 const isOpen = ref(false)
 
@@ -279,75 +273,6 @@ const formatOptions = ref<Format[]>([`rgb`, `hex`, `hsl`, `hsv`])
             }" @click="store.isUseJustify && store.useJustifyChanged()"
           >
             关闭
-          </Button>
-        </div>
-      </div>
-      <div class="space-y-2">
-        <h2>自定义 CSS 面板</h2>
-        <div class="grid grid-cols-5 justify-items-center gap-2">
-          <Button
-            class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': displayStore.isShowCssEditor,
-            }" @click="!displayStore.isShowCssEditor && customStyle()"
-          >
-            开启
-          </Button>
-          <Button
-            class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': !displayStore.isShowCssEditor,
-            }" @click="displayStore.isShowCssEditor && customStyle()"
-          >
-            关闭
-          </Button>
-        </div>
-      </div>
-      <div class="space-y-2">
-        <h2>编辑区位置</h2>
-        <div class="grid grid-cols-5 justify-items-center gap-2">
-          <Button
-            class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': store.isEditOnLeft,
-            }" @click="!store.isEditOnLeft && store.toggleEditOnLeft()"
-          >
-            左侧
-          </Button>
-          <Button
-            class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': !store.isEditOnLeft,
-            }" @click="store.isEditOnLeft && store.toggleEditOnLeft()"
-          >
-            右侧
-          </Button>
-        </div>
-      </div>
-      <div class="space-y-2">
-        <h2>预览模式</h2>
-        <div class="grid grid-cols-5 justify-items-center gap-2">
-          <Button
-            v-for="{ label, value } in widthOptions" :key="value" class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': store.previewWidth === value,
-            }" @click="store.previewWidthChanged(value)"
-          >
-            {{ label }}
-          </Button>
-        </div>
-      </div>
-      <div class="space-y-2">
-        <h2>模式</h2>
-        <div class="grid grid-cols-5 justify-items-center gap-2">
-          <Button
-            class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': !isDark,
-            }" @click="store.toggleDark(false)"
-          >
-            <Sun class="h-4 w-4" />
-          </Button>
-          <Button
-            class="w-full" variant="outline" :class="{
-              'border-black dark:border-white border-2': isDark,
-            }" @click="store.toggleDark(true)"
-          >
-            <Moon class="h-4 w-4" />
           </Button>
         </div>
       </div>
