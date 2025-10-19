9d161218585ab143f2b54e99b04092783bc769e5
Libin YANG <contact@yanglibin.info>
2025-10-19 12:14:38 +0800

    style: update icons

--- apps/web/src/components/editor/editor-header/index.vue
+++ apps/web/src/components/editor/editor-header/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ChevronDownIcon, Menu, Paintbrush, Settings } from 'lucide-vue-next'
+import { ChevronDownIcon, Menu, Palette, SlidersHorizontal } from 'lucide-vue-next'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { useDisplayStore, useStore } from '@/stores'
 import { addPrefix, generatePureHTML, processClipboardContent } from '@/utils'
@@ -269,7 +269,7 @@ async function copy() {
       <Popover>
         <PopoverTrigger as-child>
           <Button variant="outline" size="icon" class="mr-1">
-            <Paintbrush class="size-4" />
+            <SlidersHorizontal class="size-4" />
           </Button>
         </PopoverTrigger>
         <PopoverContent align="end">
@@ -283,7 +283,7 @@ async function copy() {
         size="icon"
         @click="store.isOpenRightSlider = !store.isOpenRightSlider"
       >
-        <Settings class="size-4" />
+        <Palette class="size-4" />
       </Button>
     </div>
   </header>
