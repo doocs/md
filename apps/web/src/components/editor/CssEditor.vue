bit-cook/WeChat-Markdown-Editor

fix: default css name

apps/web/src/components/CodemirrorEditor/CssEditor.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { themeOptions } from '@md/shared'
 import { Edit3, Plus, X } from 'lucide-vue-next'
 import { useDisplayStore, useStore } from '@/stores'
 
@@ -102,7 +103,7 @@ function delTab() {
 }
 
 function addHandler() {
-  addInputVal.value = `方案${store.cssContentConfig.tabs.length + 1}`
+  addInputVal.value = `方案${store.cssContentConfig.tabs.length - themeOptions.length + 1}`
   isOpenAddDialog.value = true
 }
 

doocs/md

apps/web/src/components/editor/CssEditor.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { themeOptions } from '@md/shared'
 import { Edit3, Plus, X } from 'lucide-vue-next'
 import { useCssEditorStore } from '@/stores/cssEditor'
 import { useDisplayStore } from '@/stores/display'
@@ -114,7 +115,7 @@ function delTab() {
 }
 
 function addHandler() {
-  addInputVal.value = `方案${cssContentConfig.value.tabs.length + 1}`
+  addInputVal.value = `方案${cssContentConfig.value.tabs.length - themeOptions.length + 1}`
   isOpenAddDialog.value = true
 }
 