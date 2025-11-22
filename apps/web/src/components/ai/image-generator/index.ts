099856c3862d986452b660e0d87ab348c69d6844
YangFong <yangfong2022@qq.com>
2025-11-22 17:00:43 +0800

    chore: remove unused AI component (#1162)

--- apps/web/src/components/ai/image-generator/index.ts
+++ apps/web/src/components/ai/image-generator/index.ts
@@ -1,3 +1,2 @@
 export { default as AIImageConfig } from './AIImageConfig.vue'
-export { default as AIImageGeneratorBtn } from './AIImageGeneratorBtn.vue'
 export { default as AIImageGeneratorPanel } from './AIImageGeneratorPanel.vue'
