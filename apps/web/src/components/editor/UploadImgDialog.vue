ec4c019b0fc91e6fc2444de954f488703388e980
Lyfe <9579249@qq.com>
2026-03-17 21:09:36 +0800

    feat: add useCDN option to GitHub file upload (#1376)
    Co-authored-by: Libin YANG <contact@yanglibin.info>

--- apps/web/src/components/editor/UploadImgDialog.vue
+++ apps/web/src/components/editor/UploadImgDialog.vue
@@ -18,9 +18,10 @@ const githubSchema = toTypedSchema(yup.object({
   repo: yup.string().required(`GitHub 仓库不能为空`),
   branch: yup.string().optional(),
   accessToken: yup.string().required(`GitHub Token 不能为空`),
+  useCDN: yup.boolean().required(),
 }))
 
-const githubConfig = store.reactive(`githubConfig`, { repo: ``, branch: ``, accessToken: `` })
+const githubConfig = store.reactive(`githubConfig`, { repo: ``, branch: ``, accessToken: ``, useCDN: false })
 
 async function githubSubmit(formValues: any) {
   Object.assign(githubConfig.value, formValues)
@@ -394,7 +395,7 @@ onChange(async (files) => {
 async function onDrop(e: DragEvent) {
   dragover.value = false
   e.stopPropagation()
-  const file = Array.from(e.dataTransfer!.files)[0]
+  const file = [...e.dataTransfer!.files][0]
   if (await beforeImageUpload(file)) {
     emitUploads(file)
   }
@@ -559,6 +560,17 @@ function onTabScroll(e: WheelEvent) {
                     v-model="field.value"
                     type="password"
                     placeholder="如：cc1d0c1426d0fd0902bd2d7184b14da61b8abc46"
+                  />
+                </FormItem>
+              </Field>
+
+              <Field v-slot="{ field, errorMessage }" name="useCDN" type="boolean">
+                <FormItem label="CDN 加速" :error="errorMessage">
+                  <Switch
+                    :checked="field.value"
+                    :name="field.name"
+                    @update:checked="field.onChange"
+                    @blur="field.onBlur"
                   />
                 </FormItem>
               </Field>
